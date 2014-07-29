5a380df9ca9d429493720f0e4bddd85a4784e265
poojaadani <pujas985@gmail.com>
2014-07-29 12:27:03 +0530

    Zend Install and some updates

--- Zend-Sample/init_autoloader.php
+++ Zend-Sample/init_autoloader.php
@@ -22,7 +22,7 @@ if (file_exists('vendor/autoload.php')) {
 }
 
 $zf2Path = false;
-
+define('ZF2_PATH','vendor/ZF2/');
 if (getenv('ZF2_PATH')) {           // Support for ZF2_PATH environment variable or git submodule
     $zf2Path = getenv('ZF2_PATH');
 } elseif (get_cfg_var('zf2_path')) { // Support for zf2_path directive value
