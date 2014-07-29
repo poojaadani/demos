0aad8f9bfcba44191be43a3f64d0bb52a5909536
poojaadani <pujas985@gmail.com>
2014-07-29 11:58:59 +0530

    DB Changes

--- Zend-Sample/config/autoload/global.php
+++ Zend-Sample/config/autoload/global.php
@@ -15,7 +15,7 @@
 return array(
     'db' => array(
         'driver'         => 'Pdo',
-        'dsn'            => 'mysql:dbname=stickynotes;host=localhost',
+        'dsn'            => 'mysql:dbname=kamalqfo_demo;host=localhost',
         'driver_options' => array(
             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
         ),
